99dcf479c890ec4daeaa0a9a414ccf62cfc84d56
UnciaX <unciaq@gmail.com>
2019-04-17 22:13:10 +0800

    Add FontAwesome Package

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 
 import App from './App'
 import MusicInfomation from './plugin/musicInfomation'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
 
+/* FontAwesome Icon Initial */
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+/* Plugin */
 Vue.use(MusicInfomation)
 
 /* eslint-disable no-new */
