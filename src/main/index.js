85acee135a9374c7cdc7f1f1b27eb87ba974a80b
UnciaX <unciaq@gmail.com>
2019-04-28 17:22:09 +0800

    Change Window Default Size

--- src/main/index.js
+++ src/main/index.js
@@ -20,9 +20,9 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
+    height: 560,
     useContentSize: true,
-    width: 1000,
+    width: 560,
     webPreferences: {
       webSecurity: false
     }
